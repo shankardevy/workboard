fd16f4f12218e44d02802c850488beea5a87bd5a
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-11-14 10:43:02 +0530

    Add static login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,8 @@
 "use client";
 import { usePathname } from "next/navigation";
-
-import localFont from "next/font/local";
 import Navigation from "@/components/navigation";
 
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function RootLayout({
   children,
@@ -24,17 +11,14 @@ export default function RootLayout({
 }>) {
   const pathname = usePathname();
 
-  // Check if the current page is the login page
-  const isLoginPage = pathname === "/";
+  const isHomePage = pathname === "/";
 
   return (
     <html lang="en">
       <body>
-        {!isLoginPage && <Navigation />}
+        {!isHomePage && <Navigation />}
 
-        <main className="mt-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          {children}
-        </main>
+        <main className="mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,55 @@
-// app/login/page.tsx
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      Login page
+      <Card className="w-full max-w-md p-4">
+        <CardHeader>
+          <h2 className="text-2xl font-bold text-center">WorkBoard</h2>
+          <p className="text-sm text-center text-gray-500">
+            Welcome back! Please login to your account.
+          </p>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="you@example.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <Input
+                type="password"
+                id="password"
+                placeholder="••••••••"
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full mt-4">
+              Login
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
